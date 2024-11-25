50e78d40f22808064925c2f2adf8c420e8abaccc
kenta kamihori <kmihoken@gmail.com>
2024-11-25 19:55:50 +0900

    log出力

--- spinarak.py
+++ spinarak.py
@@ -102,23 +102,21 @@ def create_booking(day_of_month, num_of_guests, location):
     try:
         # XPaths is what will likely have to be changed if this bot breaks due to the Pokemon Cafe site updating the XML responses
         # Chrome > Developer Tools > Elements > Inspect Element > Right-click selected HTML > Copy XPath
-        driver.find_element(By.XPATH, "//*[@id=\"forms-agree\"]/div/div[1]/label").click()
-        driver.find_element(By.XPATH, "//*[@id=\"forms-agree\"]/div/div[2]/button").click()
+        driver.find_element(By.XPATH, "//*[@id=\"forms-agree\"]/div/div/div[1]/label").click()
+        driver.find_element(By.XPATH, "//*[@id=\"forms-agree\"]/div/div/div[2]/button").click()
         time.sleep(random.randint(3, 6))
-        driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/a").click()
+        driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/div[1]/a").click()
         time.sleep(random.randint(3, 6))
         select = Select(driver.find_element(By.NAME, 'guest'))
         time.sleep(random.randint(2, 3))
-	
         select.select_by_index(num_of_guests)
-        #time.sleep(random.randint(5, 10))
+        time.sleep(random.randint(5, 10))
 
         # Check if the updated page indicates availability
         soup = BeautifulSoup(driver.page_source, "html.parser")
         # Find all calendar-day-cell elements
         calendar_cells = soup.find_all("li")
         #calendar_cells = soup.find_all("li", class_="calendar-day-cell")
-      
         available = False
         # Check each calendar cell for availability
         available_slots = []
